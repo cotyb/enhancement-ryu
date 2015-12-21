0c12f6d5758abb83f8ddc669e48c45af32c0247f
cotyb <xieleicotyb@gmail.com>
2015-12-21 11:26:52 +0800

    ovs recompile and reinstall
    when you change your source code in openvswith, run this shell to
    achieve recompile and reinstall

---
+++ ovs_recompile.sh
@@ -0,0 +1,20 @@
+#remove the old ovs
+aptitude remove openvswitch-common openvswitch-datapath-dkms openvswitch-controller openvswitch-pki openvswitch-switch -y
+#install the new ovs
+cd openvswitch-2.4.0
+./configure --prefix=/usr --with-linux=/lib/modules/`uname -r`/build
+make
+make install 
+make modules_install
+rmmod openvswitch
+depmod -a
+#disable openvswitch controller
+/etc/init.d/openvswitch-controller stop
+update-rc.d openvswitch-controller disable
+
+#start the new ovs
+/etc/init.d/openvswitch-switch start
+ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach --log-file
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
