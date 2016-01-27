dd7cde7678ba02f7dbe6fcbb72814e9049678794
cotyb <xieleicotyb@gmail.com>
2016-01-27 09:54:57 +0800

    start ovs before using it
    cotyb

---
+++ ovs-db connection.sh
@@ -0,0 +1,3 @@
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach --log-file
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
